a5fc0960741fb92cb1722dc5cfb57dd0c427bac4
kdimon15 <dimakucenko500@gmail.com>
2021-06-13 20:00:40 +0500

    Сделан новый baseline с github

--- Road_lane_detection/functions.py
+++ Road_lane_detection/functions.py
@@ -3,92 +3,11 @@ import pyautogui
 import numpy as np
 import cv2
 from math import sqrt
-import os
 
 
 def get_screenshot():
     screen = pyautogui.screenshot()
     return screen
-
-
-def distance(x1, y1, x2, y2):
-    return sqrt((x2-x1) ** 2 + (y2-y1)**2)
-
-
-def remove_thresh(image, thresh=100):
-    color_select = np.copy(image)
-    red_threshold = thresh
-    green_threshold = thresh
-    blue_threshold = thresh
-    rgb_threshold = [red_threshold, green_threshold, blue_threshold]
-    thresholds = (image[:, :, 0] < rgb_threshold[0]) \
-                 | (image[:, :, 1] < rgb_threshold[1]) \
-                 | (image[:, :, 2] < rgb_threshold[2])
-    color_select[thresholds] = [0, 0, 0]
-    return color_select
-
-
-def get_canny(image):  # Поиск границ
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    canny = cv2.Canny(blur, 50, 150)
-    return canny
-
-
-def region_of_interest(image):  # Fill Poly Остается все, что есть в полигоне
-    height = image.shape[0]
-    width = image.shape[1]
-    polygons = np.array([
-        [(0, height), (width, height), (500, 0), (250, 0)]
-    ])
-    mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, (255, 255, 255))
-    masked_image = cv2.bitwise_and(image, mask)
-    return masked_image
-
-
-def display_lines(image, lines):
-    line_image = np.zeros_like(image)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 7)
-    return line_image
-
-
-def create_coordinates(image, line_parameters):
-    slope, intercept = line_parameters
-    y1 = image.shape[0]
-    y2 = int(y1 * (1 / 5))
-    x1 = int((y1 - intercept) / slope)
-    x2 = int((y2 - intercept) / slope)
-    return np.array([x1, y1, x2, y2])
-
-
-def average_slope_intercept(image, lines):
-    left_fit = []
-    right_fit = []
-    l = []
-    for line in lines:
-        x1, y1, x2, y2 = line.reshape(4)
-
-        # It will fit the polynomial and the intercept and slope
-        parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        slope = parameters[0]
-        intercept = parameters[1]
-        if -6 < slope < -0.3:
-            left_fit.append((slope, intercept))
-        elif 6 > slope > 0.3:
-            right_fit.append((slope, intercept))
-    if len(left_fit) > 0:
-        left_fit_average = np.average(left_fit, axis=0)
-        left_line = create_coordinates(image, left_fit_average)
-        l.append(left_line)
-    if len(right_fit) > 0:
-        right_fit_average = np.average(right_fit, axis=0)
-        right_line = create_coordinates(image, right_fit_average)
-        l.append(right_line)
-    return np.array(l)
 
 
 def make_video():
@@ -102,47 +21,57 @@ def make_video():
     out.release()
 
 
+def white_and_yellow(image):
+    lower = np.uint8([100, 100, 100])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(image, lower, upper)
+    # yellow color mask
+    lower = np.uint8([190, 190, 0])
+    upper = np.uint8([255, 255, 255])
+    yellow_mask = cv2.inRange(image, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    masked = cv2.bitwise_and(image, image, mask=mask)
+    return masked
+
+
+def convert_to_gray(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+
+def region_of_interest(image):
+    height = image.shape[0]
+    width = image.shape[1]
+    polygons = np.array([
+        [(0, height), (width, height), (500, 0), (250, 0)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    cv2.imshow("mask", mask)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
+def blur_image(image, kernel_size=15):
+    return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+
+def detect_edges(image, low_threshold=50, high_threshold=150):
+    return cv2.Canny(image, low_threshold, high_threshold)
+
+
 def find_lines(image):
-    image = remove_thresh(image)
-    cropped_image = region_of_interest(image)
-    lane_image = np.copy(cropped_image)
-    canny = get_canny(cropped_image)
-    cropped_canny_image = region_of_interest(canny)
-    lines = cv2.HoughLinesP(cropped_canny_image, 2, np.pi / 180, 100, np.array([]), minLineLength=10, maxLineGap=80)
-    all = display_lines(lane_image, lines)
-    test_find_lines(canny, np.copy(cropped_image))
-    if lines is not None:
-        average_lines = average_slope_intercept(image, lines)
-        image_square = preprocess_lines(image, average_lines)
-    else:
-        average_lines = None
-    # line_image = display_lines(lane_image, average_lines)
-    # combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-    return cropped_image
+    selected_image = white_and_yellow(image)
+    cv2.imshow("selected", selected_image)
+    gray = convert_to_gray(selected_image)
+    cv2.imshow("gray", gray)
+    blurred_image = blur_image(gray)
+    cv2.imshow("blur", blurred_image)
+    edge_image = detect_edges(blurred_image, low_threshold=30)
+    cv2.imshow("edge", edge_image)
+    interest = region_of_interest(edge_image)
+    return interest
 
-
-def preprocess_lines(image, lines):
-    image = image.copy()
-    height, width = image.shape[:2]
-    if len(lines) == 2:
-        x1, y1, x2, y2 = lines[0].reshape(4)
-        cv2.line(image, (100, height), (x2, y2), (255, 0, 0), 7)
-
-        x1, y1, x2, y2 = lines[1].reshape(4)
-        cv2.line(image, (width-100, height), (x2, y2), (255, 0, 0), 7)
-    return image
-
-
-def test_find_lines(canny_image, image):
-    contours = cv2.findContours(canny_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
-    # Loop through my contours to find rectangles and put them in a list, so i can view them individually later.
-    cntrRect = []
-    for i in contours:
-        epsilon = 0.05 * cv2.arcLength(i, True)
-        approx = cv2.approxPolyDP(i, epsilon, True)
-        if True: # len(approx) == 4:
-            cv2.drawContours(image, cntrRect, -1, (0, 255, 0), 2)
-            cntrRect.append(approx)
 
 
 if __name__ == "__main__":
@@ -151,7 +80,7 @@ if __name__ == "__main__":
 
     while cap.isOpened():
         ret, frame = cap.read()
-        resized = frame[550:-60, 600:1320]
+        resized = frame[570:-60, 600:1320]
         # cv2.imshow("resized", resized)
         lines_image = find_lines(resized)
 
